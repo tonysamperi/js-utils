a7eab34f6b586a4f063327aa30200f0f0897d9d9
Tony Samperi <tonysamperibg89@gmail.com>
2018-04-24 22:38:13 +0200

    v 1.0.2
    * Fixed inner function reference

--- js-utils.js
+++ js-utils.js
@@ -43,14 +43,14 @@
 
     var JsUtils = (function () {
         hybridJsUtils = {
-            randomHex: function (len) {
+            randomHex: function randomHex(len) {
                 var maxlen = 8;
                 var min = Math.pow(16, Math.min(len, maxlen) - 1);
                 var max = Math.pow(16, Math.min(len, maxlen)) - 1;
                 var n = Math.floor(Math.random() * (max - min + 1)) + min;
                 var r = n.toString(16);
                 while (r.length < len) {
-                    r = r + randHex(len - maxlen);
+                    r = r + randomHex(len - maxlen);
                 }
                 return r;
             },
